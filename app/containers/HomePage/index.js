cf5d3c47801145d14958ae9d125772fce30b8292
skynbasai <skynbasai@gmail.com>
2016-06-20 15:23:43 +0800

    Add Web Component
    - SearchBar
    - LoginID
    - Password

--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -11,15 +11,56 @@
 
 import React from 'react';
 import { Link } from 'react-router';
+import TextField from 'material-ui/TextField';
+import Paper from 'material-ui/Paper'
+import styles from './style.css';
+import RaisedButton from 'material-ui/RaisedButton';
+import ActionSearch from 'material-ui/svg-icons/action/search';
+import AutoComplete from 'material-ui/AutoComplete';
+
+
 
 /* eslint-disable react/prefer-stateless-function */
 export default class HomePage extends React.Component {
 
   render() {
+    const projects = [
+      'Jinxiu',
+      'Huangsan',
+    ];
     return (
       <div>
-        <Link to="/impactMap">Map</Link>
+      	<ActionSearch className={styles.searchIcon}/>
+          <AutoComplete
+          	className={styles.searchBar}
+            hintText={'Search for Project'}
+            dataSource={projects}
+            filter={AutoComplete.fuzzyFilter}
+          />
+
+    		<Paper
+    			className={styles.ID}
+    		>
+    			
+	        <TextField
+	      		hintText="ID"
+		    	/><br />
+		    	<TextField
+		    		hintText="Password Field"
+		    		floatingLabelText="Password"
+		    		type="password"
+		    	/><br /><br></br>
+	      	<RaisedButton
+		      	label="Login"
+		      	href={'/impactMap'}
+		      	linkButton
+		      	fullWidth
+		      	primary
+	      	/>
+	      </Paper>
       </div>
     );
   }
 }
+
+
