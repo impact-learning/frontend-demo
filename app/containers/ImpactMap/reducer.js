fd7b813b260e4b9323c1e3fbc61d0bdafc094434
Terrence Lam <skyuplam@gmail.com>
2016-06-22 08:29:26 +0800

    Config socketio

--- app/containers/ImpactMap/reducer.js
+++ app/containers/ImpactMap/reducer.js
@@ -8,12 +8,14 @@ import { fromJS } from 'immutable';
 import {
   UPDATE_MAP,
   FIT_TO_BOUNDS,
+  ADD_COUNTY,
 } from './constants';
 
 const initialState = fromJS({
   zoom: 0,
   center: [],
   bounds: [],
+  counties: {},
 });
 
 function impactMapReducer(state = initialState, action) {
@@ -24,9 +26,17 @@ function impactMapReducer(state = initialState, action) {
         center: action.map.getCenter(),
       });
     }
+
     case FIT_TO_BOUNDS: {
       return state.merge({ bounds: action.bounds });
     }
+
+    case ADD_COUNTY: {
+      const county = fromJS(action.county);
+      return state.update('counties', counties =>
+        counties.set(`${county.get('township')}${county.get('village')}`, county));
+    }
+
     default:
       return state;
   }
