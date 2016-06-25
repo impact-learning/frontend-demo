aa9c259a8ccd80c7fa260dcd4bad197001a27e0e
Terrence Lam <skyuplam@gmail.com>
2016-06-25 14:34:50 +0800

    Add Fuzzyness

--- app/containers/Prediction/index.js
+++ app/containers/Prediction/index.js
@@ -10,43 +10,71 @@ import selectPrediction from './selectors';
 import styles from './styles.css';
 import ContainerDimensions from 'react-container-dimensions';
 import {
-  LineChart,
-  XAxis,
-  YAxis,
-  Line,
-  Tooltip,
-} from 'recharts';
+  VictoryChart,
+  VictoryLine,
+} from 'victory';
 
 export class Prediction extends React.Component { // eslint-disable-line react/prefer-stateless-function
+  genFuzzy(basePoints, variances) {
+    const intensity = 80;
+    const fuzzyPoints = [];
+    for (let i = 1; i <= intensity; i++) {
+      const fp1 = [];
+      basePoints.forEach((b, idx) => {
+        fp1.push({
+          x: b.x,
+          y: b.y + b.y * variances[idx] * i / intensity,
+        });
+      });
+      fuzzyPoints.push(fp1);
+    }
+    return fuzzyPoints;
+  }
+  renderLines() {
+
+  }
+
   render() {
-    const data = [
-      { name: 'Page A', uv: 4000, pv: 2400, amt: 2400 },
-      { name: 'Page B', uv: 3000, pv: 1398, amt: 2210 },
-      { name: 'Page C', uv: 2000, pv: 9800, amt: 2290 },
-      { name: 'Page D', uv: 2780, pv: 3908, amt: 2000 },
-      { name: 'Page E', uv: 1890, pv: 4800, amt: 2181 },
-      { name: 'Page F', uv: 2390, pv: 3800, amt: 2500 },
-      { name: 'Page G', uv: 3490, pv: 4300, amt: 2100 },
+    const base = [
+      { x: '2010', y: 2800 },
+      { x: '2011', y: 3000 },
+      { x: '2012', y: 2000 },
+      { x: '2013', y: 2780 },
+      { x: '2014', y: 1890 },
+      { x: '2015', y: 2390 },
+      { x: '2016', y: 3490 },
+      { x: '2017', y: 4490 },
+      { x: '2018', y: 6490 },
+      { x: '2019', y: 8490 },
+      { x: '2020', y: 9490 },
     ];
+    const variances = [0, 0, 0, 0, 0, 0, 0, 0.1, 0.17, 0.2, 0.3];
+    const data = this.genFuzzy(base, variances);
     return (
       <div className={styles.prediction}>
         <ContainerDimensions>
           {
-            ({width, height}) =>
-              <LineChart
+            ({ width }) =>
+              <VictoryChart
                 width={width}
-                height={500}
-                data={data}
+                height={450}
               >
-                <Line
-                  type="monotone"
-                  dataKey="pv"
-                  stroke="#8884d8"
-                />
-                <XAxis />
-                <YAxis />
-                <Tooltip />
-              </LineChart>
+                {
+                  data.map(d =>
+                    <VictoryLine
+                      data={d}
+                      interpolation="monotoneX"
+                      style={{
+                        data: {
+                          stroke: '#00BCD4',
+                          strokeOpacity: 0.4,
+                          opacity: 0.3,
+                        },
+                      }}
+                    />
+                  )
+                }
+              </VictoryChart>
           }
         </ContainerDimensions>
       </div>
